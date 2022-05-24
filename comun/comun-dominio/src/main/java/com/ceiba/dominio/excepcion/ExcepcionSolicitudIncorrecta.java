189d52e93a67da1004466fcd7dbefa22bcb800f2
ruben rodriguez <proyecto.rubendariorq@gmail.com>
2022-05-24 11:50:34 -0500

    Fix: Se agrega nuevo tipo de excepcion

--- comun/comun-dominio/src/main/java/com/ceiba/dominio/excepcion/ExcepcionSolicitudIncorrecta.java
+++ comun/comun-dominio/src/main/java/com/ceiba/dominio/excepcion/ExcepcionSolicitudIncorrecta.java
@@ -1,2 +1,10 @@
-package com.ceiba.dominio.excepcion;public class ExcepcionSolicitudIncorrecta {
+package com.ceiba.dominio.excepcion;
+
+public class ExcepcionSolicitudIncorrecta extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ExcepcionSolicitudIncorrecta(String message) {
+        super(message);
+    }
 }
