59abc7a4ad443d13ab7d5a16fc7f0d33334feb7a
ruben rodriguez <proyecto.rubendariorq@gmail.com>
2022-05-27 18:47:32 -0500

    Ajuste dto mascota

--- microservicio/dominio/src/main/java/com/ceiba/mascota/modelo/dto/MascotaDTO.java
+++ microservicio/dominio/src/main/java/com/ceiba/mascota/modelo/dto/MascotaDTO.java
@@ -3,10 +3,12 @@ package com.ceiba.mascota.modelo.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class MascotaDTO {
     private Long id;
     private String codigoMascota;
