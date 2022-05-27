a27d5cb0a3395d6d3a9ac41031231a0b42cf51dc
ruben rodriguez <proyecto.rubendariorq@gmail.com>
2022-05-27 17:30:05 -0500

    Se crea pruebas de integracion para el servicio tratamiento

---
+++ microservicio/dominio/src/test/java/com/ceiba/tratamiento/servicio/ServicioTratamientoTest.java
@@ -0,0 +1,33 @@
+package com.ceiba.tratamiento.servicio;
+
+import com.ceiba.tratamiento.TratamientoTestDataBuilder;
+import com.ceiba.tratamiento.modelo.entidad.SolicitudIniciarTratamiento;
+import com.ceiba.tratamiento.modelo.entidad.Tratamiento;
+import com.ceiba.tratamiento.puerto.repositorio.RepositorioTratamiento;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ServicioTratamientoTest {
+
+    @Test
+    void deberiaCrearTratamientoCorrectamente() {
+        Tratamiento tratamiento = new TratamientoTestDataBuilder()
+                .conTratamientoPorDefecto()
+                .reconstruir();
+
+        SolicitudIniciarTratamiento solicitudIniciarTratamiento = new SolicitudIniciarTratamiento(tratamiento.getMascota(), tratamiento.getServicio(), tratamiento, null);
+        RepositorioTratamiento repositorioTratamiento = Mockito.mock(RepositorioTratamiento.class);
+        Mockito.when(repositorioTratamiento.guardar(Mockito.any())).thenReturn(new TratamientoTestDataBuilder().conTratamientoPorDefecto().reconstruir());
+
+        ServicioTratamiento servicioTratamiento = new ServicioTratamiento(repositorioTratamiento);
+        Tratamiento tratamientoResp = servicioTratamiento.ejecutar(solicitudIniciarTratamiento);
+
+        Assertions.assertEquals(1l, tratamientoResp.getId());
+        Assertions.assertEquals("TRAT001", tratamientoResp.getCodigoTratamiento());
+        Assertions.assertEquals("2022-06-01", tratamientoResp.getFechaInicio().toString());
+        Assertions.assertEquals("2022-06-04", tratamientoResp.getFechaFin().toString());
+        Assertions.assertEquals(1l, tratamientoResp.getTipoTratamiento());
+        Assertions.assertEquals(49000D, tratamientoResp.getValor());
+    }
+}
